1be30c6ca3db2c5f30246ad1f5e5502cce7aea9a
Dvoryadkin Pavel <52860505+DvoryadkinPavel@users.noreply.github.com>
2019-07-18 00:49:09 +0700

    Add files via upload

---
+++ Curses.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BluntEditor
+{
+    public class Curses
+    {
+        const string NCurses = "libncursesw.so.5";
+        private IntPtr window;
+        /// <summary>
+        /// Initscr this instance.
+        /// </summary>
+        /// <returns>The initscr.</returns>
+        [DllImport(NCurses)]
+        private extern static IntPtr initscr();
+        public Curses()
+        {
+            window = initscr();
+        }
+        /// <summary>
+        /// Endwin this instance.
+        /// </summary>
+        /// <returns>The endwin.</returns>
+        [DllImport(NCurses)]
+        private extern static int endwin();
+        ~Curses()
+        {
+            int result = endwin();
+        }
+        /// <summary>
+        /// Mvwprintw the specified window, y, x and message.
+        /// </summary>
+        /// <returns>The mvwprintw.</returns>
+        /// <param name="window">Window.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="message">Message.</param>
+        [DllImport(NCurses)]
+        private extern static int mvwprintw(IntPtr window, int y, int x, string message);
+        /// <summary>
+        /// Print the specified x, y and message.
+        /// </summary>
+        /// <returns>The print.</returns>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="message">Message.</param>
+        public int Print(int x, int y, string message)
+        {
+            return mvwprintw(window, y, x, message);
+        }
+        /// <summary>
+        /// Refresh the specified window.
+        /// </summary>
+        /// <returns>The refresh.</returns>
+        /// <param name="window">Window.</param>
+        [DllImport(NCurses)]
+        private extern static int refresh(IntPtr window);
+        /// <summary>
+        /// Refresh this instance.
+        /// </summary>
+        /// <returns>The refresh.</returns>
+        public int Refresh()
+        {
+            return refresh(window);
+        }
+        /// <summary>
+        /// Wgetch the specified window.
+        /// </summary>
+        /// <returns>The wgetch.</returns>
+        /// <param name="window">Window.</param>
+        [DllImport(NCurses)]
+        private extern static int wgetch(IntPtr window);
+        /// <summary>
+        /// Gets the char.
+        /// </summary>
+        /// <returns>The char.</returns>
+        public char GetChar()
+        {
+            return (char)wgetch(window);
+        }
+        /// <summary>
+        /// Move the specified y and x.
+        /// </summary>
+        /// <returns>The move.</returns>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="x">The x coordinate.</param>
+        [DllImport(NCurses)]
+        private extern static int move(int y, int x);
+        /// <summary>
+        /// Move the specified x and y.
+        /// </summary>
+        /// <returns>The move.</returns>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        public int Move(int x, int y)
+        {
+            return move(y, x);
+        }
+        /// <summary>
+        /// Insertln this instance.
+        /// </summary>
+        /// <returns>The insertln.</returns>
+        [DllImport(NCurses)]
+        private extern static int insertln();
+        /// <summary>
+        /// Inserts the line.
+        /// </summary>
+        /// <returns>The line.</returns>
+        public int InsertLine()
+        {
+            return insertln();
+        }
+        /// <summary>
+        /// Deleteln this instance.
+        /// </summary>
+        /// <returns>The deleteln.</returns>
+        [DllImport(NCurses)]
+        private extern static int deleteln();
+        /// <summary>
+        /// Deletes the line.
+        /// </summary>
+        /// <returns>The line.</returns>
+        public int DeleteLine()
+        {
+            return deleteln();
+        }
+
+        public void DeleteFirstLine()
+        {
+            Move(0, 0);
+            DeleteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Проект псевдо-графического текстового редактора
+ * с минимальным функционалом
+ * Предусловие: нужно установить ncurses ( команда : "sudo apt-get install libncurses5-dev" )
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BluntEditor
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            int posX = 0;
+            int posY = 1;
+            var Content = new List<Dictionary<int, char>>();
+            int maxY = 0;
+            //string filename = null;
+            //try
+            //{
+            //    filename = args[0];
+            //    using (StreamReader reader = File.OpenText(filename))
+            //    {
+            //        string content = reader.ReadToEnd();
+            //        var strings = content.Split(new string[]{"\r\n"},StringSplitOptions.RemoveEmptyEntries);
+            //        foreach (var str in strings)
+            //        {
+            //            Console.WriteLine(str);
+            //        }
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.Error.WriteLine(ex);
+            //}
+            Curses Curses = new Curses();
+            Curses.Print(0, 0, "Введите текст:");
+            Curses.Move(0, 1);
+            Curses.Refresh();
+            var currentString = new Dictionary<int, char>();
+            bool exit = false;
+            while (!exit)
+            {
+                char ch = Console.ReadKey(true).KeyChar;
+                if(ch != (char)ConsoleKey.Escape)
+                {
+                    if(posX ==0)
+                    {
+                        currentString = new Dictionary<int,char>();
+                    }
+                    if (ch == (char)ConsoleKey.Enter)
+                    {
+                        currentString.Add(posX,'\r');
+                        posX++;
+                        currentString.Add(posX,'\n');
+                        posY++;
+                        posX = 0;
+                        Content.Add(currentString);
+                        maxY = Console.WindowHeight;
+                        if ((maxY-1) == posY)
+                        {
+                            Curses.DeleteFirstLine();
+                            posY--;
+                        }
+                        Curses.Print(posX, posY, ch.ToString());
+                    }
+                    else
+                    {
+                        currentString.Add(posX, ch);
+                        Curses.Print(posX, posY, ch.ToString());
+                        posX++;
+                    }
+                    Curses.Refresh();
+                }
+                else
+                {
+                    if (posX != 0)
+                    {
+                        Content.Add(currentString);
+                    }
+                    exit = true;
+                }
+            }
+            Curses = null;
+
+            Console.Clear();
+            Console.WriteLine("Result :\r");
+            foreach (var str in Content)
+            {
+                foreach(var ch in str)
+                {
+                    Console.Write(ch.Value);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
