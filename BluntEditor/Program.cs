6f70a4506b56cd0ac83f00c07f79ac3de54d51b6
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2019-07-20 02:42:53 +0700

    Delete char added

--- BluntEditor/Program.cs
+++ BluntEditor/Program.cs
@@ -38,18 +38,50 @@ namespace BluntEditor
             Curses.Refresh();
             currentString = "";
         }
+        public static List<char> ToCharList(string str)
+        {
+            var arr = currentString.ToCharArray();
+            var list = new List<char>();
+            foreach (var c in arr) list.Add(c);
+            return list;
+        }
+        public static string CharListToString(List<char> list)
+        {
+            var str = "";
+            foreach (var c in list) str+=c;
+            return str;
+        }
+        public static void RefreshLine()
+        {
+            var emptyStr = "";
+            for (int i = 0; i < maxX; i++) emptyStr += " ";
+            Curses.Print(0, posY, emptyStr);
+            Curses.Print(0, posY, currentString);
+            Curses.Move(posX, posY);
+            Curses.Refresh();
+        }
         public static void AddCharToCurrentLine(char ch)
         {
-            currentString += ch;
-            Curses.Print(posX, posY, ch.ToString());
-            posX++;
-            Curses.Refresh();
+            if(posX == currentString.Length)
+            {
+                currentString += ch;
+                Curses.Print(posX, posY, ch.ToString());
+                posX++;
+                Curses.Refresh();
+            }
+            else
+            {
+                var list = ToCharList(currentString);
+                list.Insert(posX, ch);
+                currentString = CharListToString(list);
+                RefreshLine();
+            }
         }
         public static bool IsNewLine
         {
             get
             {
-                return posX == 0;
+                return String.IsNullOrEmpty(currentString);
             }
         }
         public static bool IsLineOver
@@ -78,14 +110,26 @@ namespace BluntEditor
                     Curses.Move(posX, posY);
                     Curses.Refresh();
                 }
-                else
+                else if(posX!=0)
                 {
                     posX--;
                     Curses.Print(posX, posY, " ");
                     Curses.Move(posX, posY);
-                    currentString = currentString.Remove(currentString.Length - 1);
-                    Curses.Refresh();
+                    var list = ToCharList(currentString);
+                    list.RemoveAt(posX);
+                    currentString = CharListToString(list);
+                    RefreshLine();
                 }
+            }
+        }
+        public static void DeleteEvent()
+        {
+            if (!(posX >= currentString.Length))
+            {
+                var list = ToCharList(currentString);
+                list.RemoveAt(posX);
+                currentString = CharListToString(list);
+                RefreshLine();
             }
         }
         public static void EscapeEvent()
@@ -129,15 +173,35 @@ namespace BluntEditor
                 UpdateSizes();
                 switch(ck.Key)
                 {
-                    case ConsoleKey.LeftArrow:
-                        posX--;
+                    case ConsoleKey.PageUp:
+                    case ConsoleKey.PageDown:
+                    case ConsoleKey.Insert:
+                        break;
+                    case ConsoleKey.Home:
+                        Curses.Move(0, posY);
+                        posX = 0;
+                        Curses.Refresh();
+                        break;
+                    case ConsoleKey.End:
+                        posX = currentString.Length;
                         Curses.Move(posX, posY);
                         Curses.Refresh();
                         break;
+                    case ConsoleKey.LeftArrow:
+                        if(posX > 0)
+                        {
+                            posX--;
+                            Curses.Move(posX, posY);
+                            Curses.Refresh();
+                        }
+                        break;
                     case ConsoleKey.RightArrow:
-                        posX++;
-                        Curses.Move(posX, posY);
-                        Curses.Refresh();
+                        if(posX < currentString.Length)
+                        {
+                            posX++;
+                            Curses.Move(posX, posY);
+                            Curses.Refresh();
+                        }
                         break;
                     case ConsoleKey.Escape:
                         EscapeEvent();
@@ -147,6 +211,9 @@ namespace BluntEditor
                         break;
                     case ConsoleKey.Backspace:
                         BackspaceEvent();
+                        break;
+                    case ConsoleKey.Delete:
+                        DeleteEvent();
                         break;
                     default:
                         if (!IsLineOver)
