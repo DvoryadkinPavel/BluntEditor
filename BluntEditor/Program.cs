fcf700e8fbd01265265bf4254cca6df9e1703b04
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2019-07-21 02:10:58 +0700

    scroll columns in progress

--- BluntEditor/Program.cs
+++ BluntEditor/Program.cs
@@ -2,6 +2,9 @@
  * Проект псевдо-графического текстового редактора
  * с минимальным функционалом
  * Предусловие: нужно установить ncurses ( команда : "sudo apt-get install libncurses5-dev" )
+ * TODO:
+ * прокрутка стрелками
+ * Enter для положения курсура не в конце строки
  */
 using System;
 using System.IO;
@@ -16,6 +19,15 @@ namespace BluntEditor
 {
     class MainClass
     {
+        static string _fileName = "";
+        static string FileName
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_fileName)) return "НОВЫЙ";
+                else return _fileName;
+            }
+        }
         static Curses Curses;
         static int posX = 0;
         static int posY = 1;
@@ -23,19 +35,35 @@ namespace BluntEditor
         static int maxY = 0;
         static int maxX = 0;
         static string currentString = "";
+        static int stringIndex = 0;
+        static int previousLinesCount = 0;
+        static int previousColumnCount = 0;
         static bool exit = false;
         public static void GoToNewLine()
         {
             posY++;
+            SaveLine(stringIndex);
+            stringIndex++;
             currentString = "";
             posX = 0;
-            Content.Insert(posY - 1, currentString);
             if ((maxY - 1) == posY)
             {
+                previousLinesCount++;
                 Curses.DeleteFirstLine();
                 posY--;
+                ShowInfo();
+                Curses.Print(posX, posY, EmptyString);
+                Curses.Move(posX, posY);
+                Curses.Refresh();
             }
-            RefreshLines();
+            else
+            {
+                RefreshLines();
+            }
+        }
+        public static void MoveColumns(int value = 1)
+        {
+            RefreshLines(value);
         }
         public static List<char> ToCharList(string str)
         {
@@ -55,53 +83,82 @@ namespace BluntEditor
             get
             {
                 var emptyStr = "";
-                for (int i = 0; i < maxX; i++) emptyStr += " ";
+                for (int i = 0; i <= maxX; i++) emptyStr += " ";
                 return emptyStr;
             }
         }
+        public static string SeparatorString
+        {
+            get
+            {
+                var emptyStr = "";
+                for (int i = 0; i < maxX; i++) emptyStr += "-";
+                return emptyStr;
+            }
+        }
+        public static void ShowInfo()
+        {
+            Curses.Print(0, 0, EmptyString);
+            Curses.Print(0, maxY - 1, EmptyString);
+            Curses.Print(0, 0, $"Файл : {FileName}");
+            Curses.Print(0, maxY - 1, $"Строка : {stringIndex}");
+        }
         public static void RefreshLine()
         {
-
             Curses.Print(0, posY, EmptyString);
             Curses.Print(0, posY, currentString);
             Curses.Move(posX, posY);
             Curses.Refresh();
-            SaveLine(posY - 1);
+            SaveLine(stringIndex);
         }
-        public static void RefreshLines()
-        { 
-            for(int index=1;index < maxY;index++)
+        public static void RefreshLines(int from = 0)
+        {
+            for (int index = 1; index < maxY - 1; index++)
             {
-                if(index > Content.Count)
+                if (index > Content.Count)
                 {
                     Curses.Print(0, index, EmptyString);
                 }
                 else
                 {
-                    if(String.IsNullOrEmpty((string)Content[index - 1]))
+                    var item = (string)Content[index + previousLinesCount - 1];
+
+                    if (String.IsNullOrEmpty(item))
                     {
                         Curses.Print(0, index, EmptyString);
                     }
-                    else Curses.Print(0, index, (string)Content[index - 1]);
+                    else
+                    {
+                        if(item.Length <= from)
+                        {
+                            item = "";
+                        }
+                        else
+                        {
+                            item = item.Substring(from);
+                        }
+                        Curses.Print(0, index, EmptyString);
+                        Curses.Print(0, index, item); 
+                    }
                 }
             }
+            ShowInfo();
             Curses.Move(posX, posY);
             Curses.Refresh();
         }
-        public static void AddCharToCurrentLine(char ch)
+        public static void AddCharToCurrentLine(char ch,bool withoutScroll = true)
         {
-            if(posX == currentString.Length)
+            if((posX+previousColumnCount) == currentString.Length)
             {
                 currentString += ch;
-                Curses.Print(posX, posY, ch.ToString());
-                posX++;
+                if(withoutScroll) Curses.Print(posX, posY, ch.ToString());
+                if(withoutScroll) posX++;
             }
             else
             {
                 var list = ToCharList(currentString);
-                list.Insert(posX, ch);
+                list.Insert(posX + previousColumnCount, ch);
                 currentString = CharListToString(list);
-
             }
             RefreshLine();
         }
@@ -143,10 +200,13 @@ namespace BluntEditor
             {
                 if (String.IsNullOrEmpty(currentString))
                 {
-                    Curses.DeleteLine();
-                    Content.RemoveAt(posY - 1);
-                    currentString = (string)Content[posY - 1];
-                    RefreshLines();
+                    if (Content.Count > stringIndex + 1)
+                    {
+                        Curses.DeleteLine();
+                        Content.RemoveAt(stringIndex);
+                        currentString = (string)Content[stringIndex];
+                        RefreshLines();
+                    }
                 }
                 else
                 {
@@ -172,31 +232,53 @@ namespace BluntEditor
             }
             else Content[index] = currentString;
         }
-        public static void Main(/*string[] args*/)
+        public static void ReadFile()
         {
-
-            //string filename = null;
-            //try
-            //{
-            //    filename = args[0];
-            //    using (StreamReader reader = File.OpenText(filename))
-            //    {
-            //        string content = reader.ReadToEnd();
-            //        var strings = content.Split(new string[]{"\r\n"},StringSplitOptions.RemoveEmptyEntries);
-            //        foreach (var str in strings)
-            //        {
-            //            Console.WriteLine(str);
-            //        }
-            //    }
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.Error.WriteLine(ex);
-            //}
+            using (StreamReader reader = File.OpenText(_fileName))
+            {
+                string content = reader.ReadToEnd();
+                var strings = content.Split(new string[] { "\r" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var str in strings)
+                {
+                    var current = str.Replace("\n", String.Empty);
+                    Content.Add(current);
+                }
+            }
+        }
+        public static void SaveFile()
+        {
+            var text = "";
+            foreach (var str in Content)
+            {
+                text += str + "\r";
+            }
+            if (String.IsNullOrEmpty(_fileName))
+            {
+                Console.Clear();
+                Console.Write("Введите имя файла : ");
+                _fileName = Console.ReadLine();
+            }
+            using (StreamWriter sw = new StreamWriter(_fileName, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            if(args.Length!=0)
+            {
+                _fileName = args[0];
+                ReadFile();
+                currentString = (string)Content[0];
+            }
+            _fileName = "test.txt";//для отладки, потом убрать
+            ReadFile();//для отладки, потом убрать
+            currentString = (string)Content[0];//для отладки, потом убрать
             Curses = new Curses();
             Curses.NoEcho();
             Curses.KeyPad(true);
-            Curses.Print(0, 0, "Введите текст:");
+            UpdateSizes();
+            RefreshLines();
             Curses.Move(0, 1);
             Curses.Refresh();
             while (!exit)
@@ -222,30 +304,49 @@ namespace BluntEditor
                     case ConsoleKey.UpArrow:
                         if(posY!=1)
                         {
-                            if (posY > Content.Count)
-                            {
-                                Content.Add(currentString);
-                            }
-                            else
-                            {
-                                SaveLine(posY - 1);
-                            }
-                            posY--;
-                            currentString = (string)Content[posY - 1];
+                            SaveLine(stringIndex);
+                            posY--; stringIndex--;
+                            ShowInfo();
+                            currentString = (string)Content[stringIndex];
                             if (posX > currentString.Length) posX = currentString.Length;
                             Curses.Move(posX, posY);
                             Curses.Refresh();
                         }
+                        else
+                        {
+                            if(stringIndex!=0)
+                            {
+                                SaveLine(stringIndex);
+                                stringIndex--;
+                                previousLinesCount--;
+                                ShowInfo();
+                                currentString = (string)Content[stringIndex];
+                                RefreshLines();
+                            }
+                        }
                         break;
                     case ConsoleKey.DownArrow:
-                        if (posY < Content.Count)
+                        if (posY < Content.Count - previousLinesCount)
                         {
-                            SaveLine(posY - 1);
-                            posY++;
-                            currentString = (string)Content[posY - 1];
-                            if (posX > currentString.Length) posX = currentString.Length;
-                            Curses.Move(posX, posY);
-                            Curses.Refresh();
+                            if(posY < maxY - 2)
+                            {
+                                SaveLine(stringIndex);
+                                posY++; stringIndex++;
+                                ShowInfo();
+                                currentString = (string)Content[stringIndex];
+                                if (posX > currentString.Length) posX = currentString.Length;
+                                Curses.Move(posX, posY);
+                                Curses.Refresh();
+                            }
+                            else
+                            {
+                                SaveLine(stringIndex);
+                                stringIndex++;
+                                previousLinesCount++;
+                                currentString = (string)Content[stringIndex];
+                                if (posX > currentString.Length) posX = currentString.Length;
+                                RefreshLines();
+                            }
                         }
                         break;
                     case ConsoleKey.LeftArrow:
@@ -283,19 +384,15 @@ namespace BluntEditor
                         }
                         else
                         {
-                            GoToNewLine();
+                            AddCharToCurrentLine(ck.KeyChar,false);
+                            previousColumnCount++;
+                            RefreshLines(previousColumnCount);
                         }
                         break;
                 }
             }
             Curses = null;
-            Console.Clear();
-            Console.WriteLine("Result :\r");
-            foreach(var str in Content)
-            {
-                Console.WriteLine(str+"\r");
-            }
-            Console.ReadKey();
+            SaveFile();
         }
     }
 }
