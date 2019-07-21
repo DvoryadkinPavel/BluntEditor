6c8adb0258f0670d6f2c9ed75ead33ee63a88dae
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2019-07-22 01:44:01 +0700

    lib to config

--- BluntEditor/Curses.cs
+++ BluntEditor/Curses.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Configuration;
 
 namespace BluntEditor
 {
     public class Curses
     {
-        /// <summary>
-        /// файл библиотеки ncurses
-        /// </summary>
-        const string NCurses = "libncursesw.so.5";
         /// <summary>
         /// файл локали
         /// </summary>
@@ -17,17 +14,16 @@ namespace BluntEditor
         /// указатель на окно терминала
         /// </summary>
         private IntPtr window;
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static IntPtr initscr();
         /// <summary>
         /// конструктор класса библиотеки ncurses
         /// </summary>
         public Curses()
         {
-            SetLocale();
             window = initscr();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int endwin();
         /// <summary>
         /// Деструктор класса библиотеки ncurses
@@ -36,17 +32,7 @@ namespace BluntEditor
         {
             int result = endwin();
         }
-        [DllImport(Locale)]
-        private extern static int setlocale(int loc, string val);
-        /// <summary>
-        /// Установить локаль
-        /// </summary>
-        /// <returns>The locale.</returns>
-        public int SetLocale()
-        {
-            return setlocale(0, "");
-        }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int mvwprintw(IntPtr window, int y, int x, string message);
         /// <summary>
         /// Вывод сообщения по координатам
@@ -55,7 +41,7 @@ namespace BluntEditor
         {
             return mvwprintw(window, y, x, message);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int refresh(IntPtr window);
         /// <summary>
         /// Обновить экран
@@ -64,7 +50,7 @@ namespace BluntEditor
         {
             return refresh(window);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int getchar();
         /// <summary>
         /// Возвращает нажатую клавишу
@@ -73,7 +59,7 @@ namespace BluntEditor
         {
             return getchar();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int keypad(IntPtr window, bool bf);
         /// <summary>
         /// Включение/выключение функциональных клавиш
@@ -82,7 +68,7 @@ namespace BluntEditor
         {
             return keypad(window,bf);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int noecho();
         /// <summary>
         /// Не выводить нажатые клавиши на экран
@@ -91,7 +77,7 @@ namespace BluntEditor
         {
             return noecho();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int move(int y, int x);
         /// <summary>
         /// Переместить курсор по координатам
@@ -100,7 +86,7 @@ namespace BluntEditor
         {
             return move(y, x);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int insertln();
         /// <summary>
         /// Вставить строку
@@ -109,7 +95,7 @@ namespace BluntEditor
         {
             return insertln();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int deleteln();
         /// <summary>
         /// Удалить строку
@@ -118,7 +104,7 @@ namespace BluntEditor
         {
             return deleteln();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int start_color();
         /// <summary>
         /// Включить цвета
@@ -127,7 +113,7 @@ namespace BluntEditor
         {
             return start_color();
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int init_pair(short index, short back, short fore);
         /// <summary>
         /// Инициализация пары цветов
@@ -136,7 +122,7 @@ namespace BluntEditor
         {
             return init_pair(index, fore, back);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int COLOR_PAIR(int n);
         /// <summary>
         /// Возвращает пару цветов
@@ -145,7 +131,7 @@ namespace BluntEditor
         {
             return COLOR_PAIR(n);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int attron(int n);
         /// <summary>
         /// Применить цветовую пару
@@ -154,7 +140,7 @@ namespace BluntEditor
         {
             return attron(n);
         }
-        [DllImport(NCurses)]
+        [DllImport("CursesLib")]
         private extern static int attroff(int n);
         /// <summary>
         /// Отменить цветовую пару
